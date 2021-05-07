c930c1dc8b50f8a7f6304b50b20be0047ddffd83
ashankv <ashank.verma@gmail.com>
2021-05-07 02:26:51 -0500

    Update lifecycle

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv) {
     // int num_capitals = stoi(argv[2])    ;
 
     int T = 100;
-    int num_capitals = 20;
+    int num_capitals = 10;
 
     std::vector<double> capitals;
 
